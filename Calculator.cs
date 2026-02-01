c1603a6d71d6791c8100c074425cd3a092bf1194
dungdn <dungdnhe186806@fpt.edu.vn>
2026-02-01 11:21:11 +0700

    NET/FU: Add code implement_multiply

--- Calculator.cs
+++ Calculator.cs
@@ -6,6 +6,8 @@ private double calculate(SIGN sign, double arg1, double arg2)
             return arg1 + arg2;
         case SIGN.MINUS:
             return arg1 - arg2;
+        case SIGN.MULTIPLY:
+            return arg1 * arg2;
         default:
             return 0d;
     }
