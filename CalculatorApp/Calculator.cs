670bdf09fdd5dbfdc664668cb7f4d5b3a7bbacc9
GiaHuyhe187192 <huynghe187192@fpt.edu.vn>
2026-02-01 11:20:32 +0700

    Add Calculator base project structure

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,19 @@
+public enum SIGN
+{
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE
+}
+
+public class Calculator
+{
+    public double Calculate(SIGN sign, double arg1, double arg2)
+    {
+        switch (sign)
+        {
+            default:
+                return 0;
+        }
+    }
+}
