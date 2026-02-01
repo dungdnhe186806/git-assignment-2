aa15d44cc89f387c7dbeaf3ec967c5d97d834c5c
dungdn <dungdnhe186806@fpt.edu.vn>
2026-02-01 11:19:21 +0700

    NET/FU: Add code implement_minus

--- Calculator.cs
+++ Calculator.cs
@@ -4,6 +4,8 @@ private double calculate(SIGN sign, double arg1, double arg2)
     {
         case SIGN.PLUS:
             return arg1 + arg2;
+        case SIGN.MINUS:
+            return arg1 - arg2;
         default:
             return 0d;
     }
