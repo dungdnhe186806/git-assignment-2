ab63b75adadee539cc118dff907c56df2eb23de5
dungdn <dungdnhe186806@fpt.edu.vn>
2026-02-01 11:18:39 +0700

    NET/FU: Add code implement_plus

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,8 @@ private double calculate(SIGN sign, double arg1, double arg2)
 {
     switch (sign)
     {
+        case SIGN.PLUS:
+            return arg1 + arg2;
         default:
             return 0d;
     }
